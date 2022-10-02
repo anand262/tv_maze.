96f84a6e73e446bad45fa7a7d2508b3db1375e19
Anand_Shinde <98341248+anand262@users.noreply.github.com>
2022-10-02 19:55:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavBar from "./Component/NavBar";
+import Search from "./Component/Search";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Search />
+    </>
+  );
+}
+
+export default App;
